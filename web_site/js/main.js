524ecf97e4e2edcde89e203ccefd1d4064b44104
Mohamed Hossam <mohamed.webdev2@gmail.com>
2025-06-27 21:22:24 +0300

    Delete web_site directory

--- web_site/js/main.js
+++
@@ -1,6 +0,0 @@
-$("nav .container .nav-toggle-btn").click(function () { 
-    $("nav .container .mobile-nav-slider").toggleClass("active");
-})
-$("nav .container .nav-close-btn").click(function () { 
-    $("nav .container .mobile-nav-slider").toggleClass("active");
-})
