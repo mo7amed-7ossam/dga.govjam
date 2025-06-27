436214d804e9162ef5eebf198e2823f14e9afdc6
Mohamed Hossam <mohamed.webdev2@gmail.com>
2025-06-27 17:27:30 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$("nav .container .nav-toggle-btn").click(function () { 
+    $("nav .container .mobile-nav-slider").toggleClass("active");
+})
+$("nav .container .nav-close-btn").click(function () { 
+    $("nav .container .mobile-nav-slider").toggleClass("active");
+})
